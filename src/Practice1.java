d7b1a4a30fd5645f8df9eb20125fa9332fa1e0c3
Beyza <yildizzbeyzza@gmail.com>
2022-01-10 15:54:08 -0500

    work step 4 is done

--- src/Practice1.java
+++ src/Practice1.java
@@ -2,5 +2,6 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Step 1");
         System.out.println("Step 2");
+        System.out.println("Step 4");
     }
 }
